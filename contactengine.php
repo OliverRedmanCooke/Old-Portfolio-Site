e68f19bfb0902b0d03feb53d6473b3e9928841bf
Oliver Cooke <oliver.r.cooke@gmail.com>
2019-02-18 15:28:24 +0000

    Old Portfolio Site

---
+++ contactengine.php
@@ -0,0 +1,42 @@
+<?php
+
+$EmailFrom = "OC Media Contact Form";
+$EmailTo = "enquiries@ocmedia.co.uk, oliver.cooke11@hotmail.co.uk, oliver.r.cooke@gmail.com";
+$Subject = "OC Media Contact";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=index.html\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=index.html\">";
+}
+?>
